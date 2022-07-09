cbfeb6f355f4e7995c0d2b62245f3240f6258455
ReNayeem <renayeem@gmail.com>
2022-07-09 18:10:23 +0600

    Setup done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  return (
+    <div className="App">
+      <Toaster></Toaster>
+    </div>
+  );
+}
+
+export default App;
